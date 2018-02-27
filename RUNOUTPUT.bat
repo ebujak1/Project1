e5e1d42bf47e772f82c226dc11d5ab527d44ea0d
Edward Bujak <ebujak1@gmail.com>
2018-02-27 13:55:28 -0500

    initial submittal (fixed documentation)

---
+++ RUNOUTPUT.bat
@@ -0,0 +1,15 @@
+REM Edward Bujak
+REM Deep Learning (BBDS)
+REM Deep Learning Feed Forward Neural Network
+REM Week 2 - February 2018
+
+python --version
+
+"C:\Users\Edward Bujack\AppData\Local\Programs\Python\Python35\Python.exe" --version
+pause
+
+REM Create output.txt for full capture of periodic print statements
+python censusData.py > output.txt
+
+REM Why does this NOT work?
+REM "C:\Users\Edward Bujack\AppData\Local\Programs\Python\Python35\python.exe" -m tensorflow.tensorboard --logdir="project1"
